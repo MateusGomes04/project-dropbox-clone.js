419a0056e1f011a5df3480e30c7e1ab168bda386
MateusGomes04 <mateusgomesc04@gmail.com>
2022-10-04 11:01:05 -0300

    using ajax and promise.all to send multiple files

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -1,6 +1,6 @@
 class DropBoxController {
 
-    constructor(){
+    constructor() {
 
         this.btnSendFileEl = document.querySelector("#btn-send-file");
         this.inputFilesEl = document.querySelector("#files");
@@ -10,7 +10,7 @@ class DropBoxController {
 
     }
 
-    initEvents(){
+    initEvents() {
 
         this.btnSendFileEl.addEventListener("click", event => {
 
@@ -20,11 +20,52 @@ class DropBoxController {
 
         this.inputFilesEl.addEventListener("change", event => {
 
-            console.log(event.target.files);
+            this.uploadTask(event.target.files);
 
             this.snackModalEl.style.display = "block";
 
         });
 
+    }
+
+    uploadTask(files) {
+
+        let promises = [];
+
+        [...files].forEach(file => {
+
+            promises.push(new Promise((resolve, reject) => {
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open("POST", "/upload")
+
+                ajax.onload = event => {
+
+                    try {
+
+                        resolve(JSON.parse(ajax.responseText))
+
+                    } catch (e) {
+
+                        reject(e);
+                    }
+                };
+
+                ajax.onerror = event => {
+
+                    reject(event)
+                };
+
+                let formData = new FormData();
+
+                formData.append("input-file", file);
+
+                ajax.send(formData);
+
+            }));
+        });
+
+        return Promise.all(promises);
     }
 }
