c72a362ecc9ff90e6d332c51ead39c7b626f9061
MateusGomes04 <mateusgomesc04@gmail.com>
2022-10-06 09:01:56 -0300

    Add selection functions with CTRL and Shift

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -371,7 +371,10 @@ class DropBoxController {
         li.innerHTML = `
             ${this.getFileIconView(file)}
             <div class="name text-center">${file.originalFileName}</div>
+
             `
+            this.initEventsLi(li);
+
         return li;
     }
 
@@ -393,6 +396,62 @@ class DropBoxController {
         });
 
     }
+    initEventsLi(li) {
+
+        li.addEventListener('click', e=> {
+
+            if(e.shiftKey) {
+
+                let firstLi = this.listFilesEl.querySelector('.selected');
+
+                if (firstLi) {
+
+                    let indexStart;
+                    let indexEnd;
+                    let lis = li.parentElement.childNodes
+
+                    lis.forEach((el, index) => {
+
+                        if (firstLi === el) indexStart = index;
+                        if (li === el) indexEnd = index;
+
+                    });
+
+                    let index = [indexStart, indexEnd].sort();
+
+                    lis.forEach((el, i) => {
+
+                        if (i >= index[0] && i <= index[1]) {
+                            el.classList.add('selected');
+                        }
+
+                    });
+
+                    return true;
+
+                }
+
+                console.log(firstLi)
+
+            }
+
+            if(!e.ctrlKey) {
+
+                this.listFilesEl.querySelectorAll('li.selected').forEach(el=> {
+
+                    el.classList.remove('selected');
+
+                });
+
+            }
+
+            li.classList.toggle('selected')
+
+        })
+
+    }
+
+
 
 
 }
